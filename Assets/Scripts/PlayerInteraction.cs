ef5e05372ab7eff97130cc2a7fd094fcfeed5b80
whaleshabob <whaleshabob@gmail.com>
2026-04-07 20:39:54 -0500

    Misc, read desc
    Fixed Stamina Overlay.
    Sprint Audio adjusted to match new speed.
    Added Item Pick-Up logic.

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,28 +1,34 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using TMPro; // Important for TextMeshPro
+using TMPro;
 
 public class PlayerInteraction : MonoBehaviour
 {
     public float interactDistance = 5f;
-    public LayerMask interactLayer = ~0;
+    public LayerMask interactLayer;
     
     [Header("UI Settings")]
-    public GameObject interactPanel; // The object that turns on/off
-    public TextMeshProUGUI promptText; // The actual text component to change words
+    public GameObject interactPanel; // The "Press E" UI container
+    public TextMeshProUGUI promptText; // The text component inside the container
+
+    [Header("Booleans")]
+    public bool hasCollar = false;
 
     void Update()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
+        // Shoot ray from center of screen (First Person)
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         RaycastHit hit;
 
-        bool hitSomething = Physics.Raycast(ray, out hit, interactDistance, interactLayer);
-
-        if (hitSomething)
+        if (Physics.Raycast(ray, out hit, interactDistance, interactLayer))
         {
-            // Check for both types
+            // 1. Check for standard interactables
             DoorInteractable door = hit.collider.GetComponentInParent<DoorInteractable>();
             CellarInteractable cellar = hit.collider.GetComponentInParent<CellarInteractable>();
+            
+            // 2. Check for the items you want to "Pick Up"
+            // We use a simple tag or script name to identify the item
+            ItemObject item = hit.collider.GetComponentInParent<ItemObject>();
 
             if (door != null)
             {
@@ -33,6 +39,17 @@ public class PlayerInteraction : MonoBehaviour
             {
                 UpdateUI(true, "Press [E] to enter cellar");
                 if (Keyboard.current.eKey.wasPressedThisFrame) cellar.OpenCellar();
+            }
+            else if (item != null)
+            {
+                UpdateUI(true, "Press [E] to pick up " + item.itemName);
+                
+                if (Keyboard.current.eKey.wasPressedThisFrame)
+                {
+                    HandlePickUp(item.itemName);
+                    Destroy(item.gameObject); // Deletes the item from the scene
+                    UpdateUI(false, "");
+                }
             }
             else
             {
@@ -45,7 +62,14 @@ public class PlayerInteraction : MonoBehaviour
         }
     }
 
-    // Helper method to keep code clean
+    // This is where your booleans live
+    void HandlePickUp(string itemName)
+    {
+        if (itemName == "Collar") hasCollar = true;
+        
+        Debug.Log("Picked up " + itemName + ". Boolean set to true.");
+    }
+
     void UpdateUI(bool state, string message)
     {
         if (interactPanel != null) interactPanel.SetActive(state);
